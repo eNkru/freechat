4989e202b475e33131f78ee39cdc4915b395c0ea
Howard Ju <enkru2000@hotmail.com>
2019-10-07 11:34:51 +1300

    fix: create a new MainController when it's not available

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ class ElectronWechat {
         // On OS X it's common to re-create a window in the app when the dock icon is
         // clicked and there are no other windows open.
         if (this.mainController === null) {
-          this.mainController = new PlayerController()
+          this.mainController = new MainController()
         } else {
           this.mainController.show()
         }
